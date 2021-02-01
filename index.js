2d8b06216addb326c972426e9e71e69113b63f80
Elvis Landi <elandi13@yahoo.com>
2021-01-31 20:25:13 -0500

    commit tree e56486f9d142f39f67aa0797bfed46574d3f34fc

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const testVar = "hi"
+
+ul.removeChild(ul.querySelector('li:nth-child(2)')) 
+const newHeader = document.createElement("hi");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = 'victory'
